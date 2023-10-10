7b3a91474bcbd9941b0e3a949bdfe3a89c8fb28d
Md Siam Uddin <siamcodeback@gmail.com>
2023-10-11 00:15:47 +0600

    First git commit

---
+++ layouts/default.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+                <NuxtLink class="navbar-brand" to="">Nuxt Dojo</NuxtLink>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+                        <li class="nav-item">
+                            <NuxtLink class="nav-link active" aria-current="page" to="/">Home</NuxtLink>
+                        </li>
+
+                        <li class="nav-item" v-for="cat in categorys" :key="cat.id">
+                            <NuxtLink class="nav-link" :to="`/category/${ cat }`" >{{ cat }}</NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <slot />
+        </div>
+
+    </div>
+</template>
+<script setup>
+    useHead({
+        link:[
+            {
+            href:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+            rel:"stylesheet",
+            integrity:"sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN",
+            crossorigin:"anonymous"
+            } 
+        ],
+
+        script:[
+            {
+                src:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+                integrity:"sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL",
+                crossorigin:"anonymous"
+            }
+        ]
+    })
+
+    let catgory_url = 'https://fakestoreapi.com/products/categories'
+
+    const { data:categorys } = await useFetch(catgory_url);
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+    
+        <div id="carouselExampleIndicators" class="carousel slide mt-2" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="~/assets/images/slider-1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="~/assets/images/slider-2.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="~/assets/images/slider-3.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+        <div class="row product-row">
+            <div class="col-12 mt-4 mb-4">
+                <h1>Just For You</h1>
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 product-main " v-for="product in products" :key="product.id">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <img :src="product.image" :alt="product.title" class="img-fluid proudct-image">
+                        <h4 class="mt-2">{{ product.title.slice(0, 30) }}</h4>
+                        <h5>{{ product.price }}</h5>
+                        <center><NuxtLink class="btn btn-success btn-sm" :to="`/product/${product.id}`">View Details</NuxtLink></center>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script setup>
+    definePageMeta({
+        layout:'default',
+    });
+
+    let url = 'https://fakestoreapi.com/products'
+    const {data:products} = await useFetch(url);
+
+    useHead({
+        title: "Home | Dojo"
+    })
+</script>
+
+<style scoped>
+    .proudct-image{
+        width: 100%;
+        height: 250px;
+    }
+
+    .product-main{
+        height: 400px;
+    }
+
+    .product-row{
+        row-gap: 40px;
+    }
+</style>
